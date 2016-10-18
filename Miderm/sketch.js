8e06a19dc5a1ef22aa9af1602c46ff03e03d92d8
aka5hkumar <mykrapymail@gmail.com>
2016-10-17 21:14:19 -0400

    Working point system

--- Miderm/sketch.js
+++ Miderm/sketch.js
@@ -13,16 +13,14 @@ var shape3;
 var score = 0;
 var circleColor;
 var characterColor = 255;
-var i;
-var j;
-var k;
-var clock=60;
+var spriteX = 250;
+var spriteY = 400;
 
 function setup() {
     createCanvas(500, 500)
     background(255);
-   randoms();
-   circleColor = color(255, 0, 0);
+    randoms();
+    circleColor = color(255, 0, 0);
 }
 
 function draw() {
@@ -50,16 +48,16 @@ function mainScreen() {
 
     background(156);
     fill(0);
+    hit();
 
-
-  //  println(randoms());
+    //  println(randoms());
     shapes();
     fill(0);
-        rect(0, 450, 500, 50);
+    rect(0, 450, 500, 50);
     fill(255);
     text(score, 480, 480);
     text(lives, 20, 480);
-      println (shape1, shape2, shape3, shapeLocation1, shapeLocation2, shapeLocation3);
+    println(shape1, shape2, shape3, shapeLocation1, shapeLocation2, shapeLocation3);
     //Background image
     //build sprite
     //Load shapes
@@ -79,98 +77,62 @@ function endScreen() {
 }
 // ////////////////////////////////////////Steering functions////////////////////
 function randoms() {
-  shape1 = int(random(0, 2));
-  shape2 = int(random(0, 2));
-  shape3 = int(random(0, 2));
-  shapeLocation1 = int(random(50,150));
-  shapeLocation2 = int(random(200, 300));
-  shapeLocation3 = int(random(350, 450));
+    shape1 = int(random(0, 2));
+    shape2 = int(random(0, 2));
+    shape3 = int(random(0, 2));
+    shapeLocation1 = int(random(50, 150));
+    shapeLocation2 = int(random(200, 300));
+    shapeLocation3 = int(random(350, 450));
 
 
     //Reset the random numbers on call
 }
 
 function shapes() {
-  var time = int(millis());
-  println(time);
-  speed=(time%5000);
-        if (shape1 == 0) {
-            fill(circleColor);
-            ellipse(shapeLocation1, (time%5000)/(shapeLocation1/20) , 50,50)
-            if (speed>4500)
-            {
-              randoms();
-            }
-        } else if (shape1 == 1) {
-            fill(0, 0, 255);
-          rect(shapeLocation1,(time%5000)/10, 50,50);
-          if (speed>4500)
-          {
+    var time = int(millis());
+    println(time);
+    speed = (time % 5000);
+    if (shape1 == 0) {
+        fill(circleColor);
+        ellipse(shapeLocation1, (time % 5000) / (shapeLocation1 / 20), 50, 50)
+        if (speed > 4500) {
             randoms();
-          }
-      }
-      if (shape2 == 0) {
-          fill(circleColor);
-          ellipse(shapeLocation2, (time%5000)/(shapeLocation2/30) , 50,50)
-          if (speed>4500)
-          {
+        }
+    } else if (shape1 == 1) {
+        fill(0, 0, 255);
+        rect(shapeLocation1, (time % 5000) / 10, 50, 50);
+        if (speed > 4500) {
             randoms();
-          }
-      } else if (shape2 == 1) {
-          fill(0, 0, 255);
-        rect(shapeLocation2,(time%5000)/(shapeLocation2/30), 50,50);
-        if (speed>4500)
-        {
-          randoms();
+        }
+    }
+    if (shape2 == 0) {
+        fill(circleColor);
+        ellipse(shapeLocation2, (time % 5000) / (shapeLocation2 / 30), 50, 50)
+        if (speed > 4500) {
+            randoms();
+        }
+    } else if (shape2 == 1) {
+        fill(0, 0, 255);
+        rect(shapeLocation2, (time % 5000) / (shapeLocation2 / 30), 50, 50);
+        if (speed > 4500) {
+            randoms();
         }
     }
     if (shape1 == 0) {
         fill(circleColor);
-        ellipse(shapeLocation3, (time%5000)/(shapeLocation3/50) , 50,50)
-        if (speed>4800)
-        {
-          randoms();
+        ellipse(shapeLocation3, (time % 5000) / (shapeLocation3 / 50), 50, 50)
+        if (speed > 4800) {
+            randoms();
         }
     } else if (shape1 == 1) {
         fill(0, 0, 255);
-      rect(shapeLocation3,(time%5000)/(shapeLocation3/50), 50,50);
-      if (speed>4500)
-      {
-        randoms();
-      }
-  }
-    // for (j = 0; j < width; j+=1) {
-    //     if (shape2 == 0) {
-    //         fill(circleColor);
-    //         ellipse(shapeLocation2, 100+time%400, 50,50)
-    //     } else if (shape2 == 1) {
-    //         fill(0, 0, 255);
-    //         rect(shapeLocation2, 100+time%400, 50,50)
-    //     }
-    // }
-    // for (k = 0; k < width; k+=1) {
-    //     if (shape3 == 0) {
-    //         fill(circleColor);
-    //         ellipse(shapeLocation3,200+time&300, 50,50)
-    //     } else if (shape3 == 1) {
-    //         fill(0, 0, 255);
-    //         rect(shapeLocation3, 200+time&300, 50,50)
-    //     }
-    // }
-    if (i > width) {
-        i == 0;
-      //  randoms();
+        rect(shapeLocation3, (time % 5000) / (shapeLocation3 / 50), 50, 50);
+        if (speed > 4500) {
+            randoms();
+        }
     }
-    //
-    // if (j > width) {
-    //     j == 0;
-    //     randoms();
-    // }
-    // if (k > width) {
-    //     k == 0;
-    //     randoms();
-    // }
-    // //println(i,j.k);
+    fill(time%255);
+    ellipse(spriteX, spriteY, 50, 80);
     //ellipse random location
     //triangle random location
 
@@ -185,6 +147,32 @@ function movement() {
 }
 
 function hit() {
+    if (spriteX == shapeLocation1) {
+        if (shape1 == 0) {
+            score += 10;
+        } else if (shape1 == 1) {
+            score -= 10;
+        }
+    }
+    if (spriteX == shapeLocation2) {
+        if (shape2 == 0) {
+            score += 10;
+        } else if (shape2 == 1) {
+            score -= 10;
+        }
+    }
+    if (spriteX == shapeLocation3) {
+
+            if (shape3 == 0)
+{
+                score += 10;
+        } else if (shape3 == 1) {
+            score -= 10;
+        }
+    }
+    if (score < 0); {
+        gameScreen == 2;
+    }
     //if shapes location matches sprite location then hit++
     /*
     if hit is green circle, then points
