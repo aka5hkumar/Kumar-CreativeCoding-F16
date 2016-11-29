bd21f2ab64474e7c03f23a7d01ae963ad1bb0403
Akash Kumar <aka5hkumar@users.noreply.github.com>
2016-11-29 07:23:26 -0500

    Tried to fix

--- cc/sketch.js
+++ cc/sketch.js
@@ -10,14 +10,37 @@ var url;
 var sunrise;
 var sunset;
 var cityNum;
+var globalData;
 
 function setup() {
-        var inputValues = "/assets/cities.json";
-    inputData = loadJSON(inputValues);
-    cityNum = 1;
+    var inputValues = ("/assets/cities.json");
+    inputData = loadJSON(inputValues, getData);
+    console.log(inputData.cities);
     createCanvas(600, 600);
-    sunValues = "http://api.sunrise-sunset.org/json?";
-   city = inputData.cities[cityNum].name;
+    cityNum = 0;
+
+}
+
+function draw() {
+    sunData(cityNum);
+    text("In", city);
+    text("Sunrise is", sunrise);
+    text("and sunset is", sunset);
+    text("Click to load another city");
+}
+
+function getData(inputData) {
+
+    globalData = inputData;
+    console.log(globalData);
+
+}
+
+
+function sunData(whatCity) {
+    whatCity = this.cityNum;
+    sunValues = ("http://api.sunrise-sunset.org/json?");
+    city = inputData.cities[cityNum].name;
     lat = inputData.cities[cityNum].lat;
     long = inputData.cities[cityNum].long;
     offset = inputData.cities[cityNum].time_off;
@@ -28,30 +51,11 @@ function setup() {
     sunset = sunset + offset;
     console.log(sunrise);
     console.log(sunset);
-
-    sunData = loadJSON(url);
-
+    sunData = loadJSON(url, getData);
 }
 
-
-
-function draw() {
-    text(city, sunrise, sunset);
-}
 
 function mouseClicked() {
     cityNum++
-     city = inputData.cities[cityNum].name;
-    lat = inputData.cities[cityNum].lat;
-    long = inputData.cities[cityNum].long;
-    offset = inputData.cities[cityNum].time_off;
-    url = sunValues + "lat=" + lat + "&lng=" + long + "&date=today%20";
-    sunrise = sunValues.results.sunrise;
-    sunrise = sunrise + offset;
-    sunset = sunValues.results.sunrise;
-    sunset = sunset + offset;
-    console.log(sunrise);
-    console.log(sunset);
-    sunData = loadJSON(url);
-
+    sunData(cityNum);
 }
