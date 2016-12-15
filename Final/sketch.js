2c2c520c1883cfe825e2830f15f35c03c84c9b3a
Akash Kumar <aka5hkumar@users.noreply.github.com>
2016-12-14 20:35:26 -0500

    p5play working
    Screen selector implemented

--- Final/sketch.js
+++ Final/sketch.js
@@ -1,21 +1,48 @@
+var whatScreen;
 var whichBlock;
 var keyList;
+var player;
 
 function preload() {
     A = loadSound("assets/A.mp3");
     B = loadSound("assets/B.mp3");
     C = loadSound("assets/C.mp3");
     keyList = [A, B, C];
+    player = createSprite(((window.innerWidth) / 2), ((window.innerHeight) / 2), 64, 64);
 }
 
 function setup() {
+    whatScreen = 0;
     whichBlock = [0, 0, 0, 0];
     createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-    startScreen();
+    screenChoose();
+}
 
+function screenChoose() {
+    if (whatScreen == 0) {
+        startScreen();
+    } else if (whatScreen == 1) {
+        mainScreen();
+    } else if (whatScreen == 2) {
+        finalScreen();
+    } else if (whatScreen == 3) {
+        endScreen();
+    }
+}
+
+function mainScreen() {
+    background(255, 0, 0);
+}
+
+function finalScreen() {
+    console.log("Commence the camera")
+}
+
+function endScreen() {
+    text('fin');
 }
 
 function startScreen() {
@@ -29,6 +56,12 @@ function startScreen() {
     rect(3 * innerOffset, height / 2, demoRectSize, demoRectSize);
     fill(0, 0, 255, 200);
     rect(5 * innerOffset, height / 2, demoRectSize, demoRectSize);
+}
+
+function keyPressed() {
+    if (whatScreen == 0) {
+        whatScreen = 1;
+    }
 }
 
 function mouseClicked() {
